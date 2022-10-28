91f441208a5b46c02062570311aeb1c46f81f88e
Temateu Roslyn <temateuroslynf32@gmail.com>
2022-10-29 00:38:31 +0100

    Fin des exercices du chapitre 01

--- src/main/scala/modules/chapter01/src/main/scala/exercices/ExerciceChapter01.scala
+++ src/main/scala/modules/chapter01/src/main/scala/exercices/ExerciceChapter01.scala
@@ -3,6 +3,13 @@ package chapter01.exercices
 object ExerciceChapter01 {
 
   def run: Unit =
-    println("\n2 - Exercice du chapitre 01 :")
+    println("\n\nB - Exercice du chapitre 01 :")
     println("    -------------------------\n")
+    print("\n   1-) Définition de mot clés: \n\tJVM: JAVA VIRTUAL MACHINE \n\tJRE: JAVA RUNTIME ENVIRONNMENT \n\tJDK: JAVA DEVELOPMENT KIT \n\tBytecode: Code executable par une JVM")
+    print("\n   2-) Pourquoi Scala a été dévéloppé: \n\tA la base Scala a été dévéloppé pour concurrencer Java qui était très lent.")
+    print("\n   3-) Apache Spark ?: \n\tC'est un framework open source de calcul distribué. et aujourd'hui partie intégrante de la fondation Apache.")
+    print("\n   4-) Les cas d'usage du Big Data ?: \n\t- Les grosses bases de données.\n\t- Les dispositifs informatiques et plus largement les technologies utilisées pour gérer les gros volumes de données.\n\t- L'historique du Big Data\n\t- Développer le chiffre d'affaire: mieux annalyser les incidents causés pour anticiper l'avenir")
+    print("\n   5-) Les cas d'application de Scala ?: \n\t- Traitement de l'infomation.\n\t- Informatique distribué.\n\t- Développement Web")
+    print("\n   6-) Les produits développés en Scala ?: \n\t- Le backend de la plateforme Coursera")
+    print("\n   7-) Les performances de Python ?: \n\t- Il faut savoir que Python est un langage dit « interprété ». Cela veut dire que votre \n\tcode est transmis d’abord à une machine virtuelle connue sous le nom d’interpréteur de bytecode, avant d’être compilé.\n\t- De plus, Python stocke ses attributs dans un dictionnaire. \n\t- Enfin, il faut savoir que Python est peu favorable au multithreading")
 }
