df2f04661176e0b34d63f6c6b3b134863f46a24e
Temateu Roslyn <temateuroslynf32@gmail.com>
2022-10-28 23:05:13 +0100

    Note pour le chapitre 01 okay

--- src/main/scala/modules/chapter01/src/main/scala/exercices/ExerciceChapter01.scala
+++ src/main/scala/modules/chapter01/src/main/scala/exercices/ExerciceChapter01.scala
@@ -3,5 +3,6 @@ package chapter01.exercices
 object ExerciceChapter01 {
 
   def run: Unit =
-    println("\n2 - Exercice du chapitre 01 : \n")
+    println("\n2 - Exercice du chapitre 01 :")
+    println("    -------------------------\n")
 }
--- src/main/scala/modules/chapter01/src/main/scala/notes/NoteChapter01.scala
+++ src/main/scala/modules/chapter01/src/main/scala/notes/NoteChapter01.scala
@@ -3,6 +3,13 @@ package chapter01.notes
 object NoteChapter01 {
 
   def run: Unit =
-    println("\n1 - Notes pour le chapitre 01 : \n")
-    println()
+    println("\n1 - Notes pour le chapitre 01 :")
+    println("    ---------------------------\n")
+    println("\n> Scala: \n\tC'est un language de programmation basé sur la machine Virtuelle java (JVM) evolutit \n\tcombinant la puissance des languages Orienté objet et fonctionnels")
+    println("\n> Pourquoi apprendre Scala: \n\tA part son utilisation avec Spark dans le Big Data, Il permet d'ecrire \n\tdes programme tres simplement.")
+    println("\n> Scala et Java: \n\tJava est l'un des languages de programmation les plus populaire, multi-plateforme \n\tet est à la base de l'ecosystème Hadoop.")
+    println("\tScala est aussi basé sur la JVM de JAVA, donc un code scala compilé peut etre execute sur toute les \n\tplateforme au meme titre que JAVA.")
+    println("\tComme Scala est un language de la JVM, alors il a acces à toutes les libraries du language JAVA.")
+    println("\n> Le typage en Scala: \n\tScala est l'unique laguage à Typage Statique mais souvent flexible et Dynamique.")
+    println("\n> Apache Sparck et Scala: \n\tLa librairie Spark est écrite en Scala. Entre ces deux outils, la \n\tcommunication est harmonieuse et performente.\n\n")
 }
