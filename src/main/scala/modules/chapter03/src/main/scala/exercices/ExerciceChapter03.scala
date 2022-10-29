32816f5f3718b2db8958cf4fbb48c4980b8b7db0
Temateu Roslyn <temateuroslynf32@gmail.com>
2022-10-29 16:06:16 +0100

    Fin des exercices du chapitres 3

--- src/main/scala/modules/chapter03/src/main/scala/exercices/ExerciceChapter03.scala
+++ src/main/scala/modules/chapter03/src/main/scala/exercices/ExerciceChapter03.scala
@@ -15,15 +15,12 @@ class ExerciceChapter03 extends CustomParent  {
 
     println("\n\nB - Exercice du chapitre 03 :")
     println("    -------------------------\n")
+
     writeLog(message, Log.chapter03_log_path)
 
-    // print("\n   1-) Définition de mot clés: \n\tJVM: JAVA VIRTUAL MACHINE \n\tJRE: JAVA RUNTIME ENVIRONNMENT \n\tJDK: JAVA DEVELOPMENT KIT \n\tBytecode: Code executable par une JVM")
-    // print("\n   2-) Pourquoi Scala a été dévéloppé: \n\tA la base Scala a été dévéloppé pour concurrencer Java qui était très lent.")
-    // print("\n   3-) Apache Spark ?: \n\tC'est un framework open source de calcul distribué. et aujourd'hui partie intégrante de la fondation Apache.")
-    // print("\n   4-) Les cas d'usage du Big Data ?: \n\t- Les grosses bases de données.\n\t- Les dispositifs informatiques et plus largement les technologies utilisées pour gérer les gros volumes de données.\n\t- L'historique du Big Data\n\t- Développer le chiffre d'affaire: mieux annalyser les incidents causés pour anticiper l'avenir")
-    // print("\n   5-) Les cas d'application de Scala ?: \n\t- Traitement de l'infomation.\n\t- Informatique distribué.\n\t- Développement Web")
-    // print("\n   6-) Les produits développés en Scala ?: \n\t- Le backend de la plateforme Coursera")
-    // print("\n   7-) Les performances de Python ?: \n\t- Il faut savoir que Python est un langage dit « interprété ». Cela veut dire que votre \n\tcode est transmis d’abord à une machine virtuelle connue sous le nom d’interpréteur de bytecode, avant d’être compilé.\n\t- De plus, Python stocke ses attributs dans un dictionnaire. \n\t- Enfin, il faut savoir que Python est peu favorable au multithreading\n\n")
-    
+    print("\n   1-) Utilisation de la tabulation: \n\tDans le cas des entiers ou des chaine, la tabulation permet d'avoir toute les information sur le type d'une variale.")
+    print("\n   2-) Exemple de commade: \n\t:sh  Ex: :sh ls -al \n\t:load Ex: :load /path/to/file")
+    print("\n   3-) spark-shell ?: \n\tLes commandes son disponibles sur spark-shell egalement.")
+    print("\n   4-) Augmenter la mémoire de scala ?: \n\tIl faut utiliser la variable JAVA_OPTS qui speciafie la taille de JVM utilise a la compilation\n\n")    
 
 }
--- src/main/scala/modules/chapter03/src/main/scala/notes/NoteChapter03.scala
+++ src/main/scala/modules/chapter03/src/main/scala/notes/NoteChapter03.scala
@@ -5,9 +5,5 @@ object NoteChapter03 {
   def run: Unit =
     println("\nA - Notes pour le chapitre 03 :")
     println("    ---------------------------\n")
-    print("\n   1-) REPL: \n\tC'est le shel sur lequel on peut saisir des commandes Scala, pour y acceder: dans un terminal taper: scala")
-    print("\n   2-) Le Paste mode dans le REPL: \n\tIl permet d'ecrire nos script scala sur plusieurs ligne,pour y aller: :paste pour l'arreter: Ctrl + D.")
-    print("\n   3-) L'historique du REPL: \n\tPour y acceder: :history.")
-    print("\n   4-) Auto complete dans le REPL: \n\tIl suffit de commancer la saisie et de taper la touche tabulation.")
-    print("\n   5-) Quitter le REPL: \n\tPour quitter le REPL il faut saisir la commande: :quit.\n\n")
+    print("\n   Les notes pour ce chapitres serons bientot disponibles\n\n")
 }
