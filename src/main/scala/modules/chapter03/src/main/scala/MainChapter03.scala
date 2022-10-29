fe98d6f21d29da0cc8952275c831e878b482b955
Temateu Roslyn <temateuroslynf32@gmail.com>
2022-10-29 09:00:06 +0100

    Fin des notes sur le chapitre 03

--- src/main/scala/modules/chapter03/src/main/scala/MainChapter03.scala
+++ src/main/scala/modules/chapter03/src/main/scala/MainChapter03.scala
@@ -1,10 +1,13 @@
 package chapter03
 
+import  chapter03.notes.NoteChapter03
+import  chapter03.exercices.ExerciceChapter03
 object MainChapter03 {
   val message = "Hello from Chapter 03, I was compiled with Scala 3! :)"
 
   def run: Unit =
     println("==================  Debut de l'execution du chapitre 03  ==========================\n")
-    println(message)
+    NoteChapter03.run
+    ExerciceChapter03.run
     println("\n==================  Fin de l'execution du chapitre 03  ==========================")
 }
