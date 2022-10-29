d1b23d57b08e62471f83b42c57e59f00139a4019
Temateu Roslyn <temateuroslynf32@gmail.com>
2022-10-29 02:10:59 +0100

    Fin des exercices du chapitre 2

--- src/main/scala/modules/chapter02/src/main/scala/exercices/ExerciceChapter02.scala
+++ src/main/scala/modules/chapter02/src/main/scala/exercices/ExerciceChapter02.scala
@@ -5,11 +5,8 @@ object ExerciceChapter02 {
   def run: Unit =
     println("\n\nB - Exercice du chapitre 02 :")
     println("    -------------------------\n")
-    print("\n   1-) Définition de mot clés: \n\tJVM: JAVA VIRTUAL MACHINE \n\tJRE: JAVA RUNTIME ENVIRONNMENT \n\tJDK: JAVA DEVELOPMENT KIT \n\tBytecode: Code executable par une JVM")
-    print("\n   2-) Pourquoi Scala a été dévéloppé: \n\tA la base Scala a été dévéloppé pour concurrencer Java qui était très lent.")
-    print("\n   3-) Apache Spark ?: \n\tC'est un framework open source de calcul distribué. et aujourd'hui partie intégrante de la fondation Apache.")
-    print("\n   4-) Les cas d'usage du Big Data ?: \n\t- Les grosses bases de données.\n\t- Les dispositifs informatiques et plus largement les technologies utilisées pour gérer les gros volumes de données.\n\t- L'historique du Big Data\n\t- Développer le chiffre d'affaire: mieux annalyser les incidents causés pour anticiper l'avenir")
-    print("\n   5-) Les cas d'application de Scala ?: \n\t- Traitement de l'infomation.\n\t- Informatique distribué.\n\t- Développement Web")
-    print("\n   6-) Les produits développés en Scala ?: \n\t- Le backend de la plateforme Coursera")
-    print("\n   7-) Les performances de Python ?: \n\t- Il faut savoir que Python est un langage dit « interprété ». Cela veut dire que votre \n\tcode est transmis d’abord à une machine virtuelle connue sous le nom d’interpréteur de bytecode, avant d’être compilé.\n\t- De plus, Python stocke ses attributs dans un dictionnaire. \n\t- Enfin, il faut savoir que Python est peu favorable au multithreading\n\n")
+    print("\n   1-) Installer Scala sur Linux sans Connexion: \n\t- Pour ce faire il suffit d'avoir l'archive de sbt a disposition puis de la \n\tdécompresser dans votre répertoire de travail")
+    print("\n   2-) scala -help: \n\tAffiche dans la console toute les commandes accessible via le REPL.")
+    print("\n   3-) Plusieur version de scala ?: \n\t- Oui c'est possible, Exemple d'installation: cs install scala:2.11.12.")
+    print("\n   4-) Changement apportes dans la dernière version de Scala ?: \n\t- La Syntaxe: \n\t\t> Nouveau mot clé: `optional` \n\t\t> Nouvelle syntaxe pour les structure de control \n\t- Programmation Orientéé Object: \n\t\t> Plan pour l'heritage \n\t\t> Compostion sur l'Héritage \n\t\t> Cacher les details de l'implementation\n\n")
 }
